4e7249879c2fba59bd85f8442b60f9cf56a0975c
ntraja11 <ntraja11@gmail.com>
2025-03-25 17:01:09 +0100

    Confirmation modal implemented for delete action

---
+++ TrackYourTrip/wwwroot/js/common.js
@@ -0,0 +1,7 @@
+﻿function ShowConfirmationModal() {
+    bootstrap.Modal.getOrCreateInstance(document.getElementById("confirmationModal")).show();
+}
+
+function HideConfirmationModal() {
+    bootstrap.Modal.getOrCreateInstance(document.getElementById("confirmationModal")).hide()
+}
